fdfef934309e809a0ec58128dc17de305bbe1cdc
Gustavogits <gustavosilvagit01@gmail.com>
2026-06-10 18:52:54 -0300

    Add files via upload

---
+++ ti.js
@@ -0,0 +1,15 @@
+import leia, { questionInt } from "readline-sync"
+
+// var senhaPadrao = "1234";
+// var senha = leia.question("DIGITE SUA SENHA: ");
+
+// if(senha === senhaPadrao){
+//     console.log("ACESSO PERMITIDO")
+// } else {
+//     console.log("ACESSO NEGADO")
+// }
+
+
+var idade = leia;questionInt("DIGITE A SUA IDADE: ");
+
+console.log("A SUA IDADE É " + idade);
